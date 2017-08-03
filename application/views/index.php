05658cb5ccc8b564c4dc62684e09678ee482453a
sebastianHondarza <sebastian.hondarza@gmail.com>
2017-08-03 17:45:07 -0400

    buscar funcionando

--- application/views/index.php
+++ application/views/index.php
@@ -1,5 +1,37 @@
 <script>
 	jQuery(document).ready(function($) {
+
+		function buscador(dato){
+			$.ajax({
+				url: '<?php echo base_url();?>Portada/buscador',
+				type: 'POST',
+				data: {buscar: dato},
+				success: function (data){					
+					var registros = eval(data);
+					html = "";
+					html = "<table class='table table-bordered'><thead class='bg-primary'>\
+					<th>#</th><th>Nombre</th><th>Apellido</th><th>-</th></thead><tbody>";
+					for (var i = 0; i < registros.length; i++) {
+						html += "<tr>\
+						<td>"+registros[i]["id"]+"</td>\
+						<td>"+registros[i]["nombre"]+"</td>\
+						<td>"+registros[i]["apellido"]+"</td>\
+						<td><button id='"+registros[i]["id"]+"' class='btnModificar btn btn-xs btn-warning'>Modificar</button>&nbsp;\
+						<a href='<?php echo base_url();?>portada/eliminar/"+registros[i]["id"]+"' id='"+registros[i]["id"]+"' class='btnEliminar btn btn-xs btn-danger'>Eliminar</a></td>\
+						</tr>";
+					}
+					html += "</tbody></table>";
+					$('#resp').html(html);
+				}
+			});
+			
+		}
+		
+		$('#txtBuscar').keyup(function() {	
+			var dato = $('#txtBuscar').val();
+			buscador(dato);			
+		});
+
 		$('#btnNuevo').click(function(event) {
 			event.preventDefault();
 			$('#txtId').val('');
@@ -8,7 +40,7 @@
 			$('#modal-nuevo').modal('show');
 		});
 
-		$('.btnEliminar').click(function(event) {
+		$("html").on('click','.btnEliminar', function(event){ 
 			event.preventDefault();
 			var ruta = $(this).attr('href');
 			if (confirm("Desea borrar este registro?")) {
@@ -16,11 +48,11 @@
 			}
 		});
 
-		$('.btnModificar').click(function(event) {
+		$("html").on('click','.btnModificar', function(event){
 			event.preventDefault();
 			var id = this.id;
 			$.ajax({
-				url: '<?php echo base_url();?>/portada/get_user/'+id,
+				url: '<?php echo base_url();?>portada/get_user/'+id,
 				type: 'POST',
 				dataType: 'JSON',
 				data: {
@@ -42,7 +74,7 @@
 <div class="row">
 	<div class="col-sm-3">
 		<div class="input-group">
-		<input type="text" class="form-control" placeholder="..." autofocus="">
+		<input type="text" class="form-control" name="txtBuscar" id="txtBuscar" placeholder="..." autofocus="">
 			<div class="input-group-addon">
 				<i class="glyphicon glyphicon-search"></i>
 			</div>
@@ -58,29 +90,32 @@
 		<button class="btn btn-success" id="btnNuevo">Nuevo</button>	
 	</div>
 </div>
-<table class="table table-bordered">
-	<thead class="bg-primary">
-		<th>#</th>
-		<th>Nombre</th>
-		<th>Apellido</th>
-		<th>-</th>		
-	</thead>
-	<tbody>
-		<?php 
-		foreach ($arr as $key) {
-			echo "<tr>";
-		 	echo "<td>".$key->id."</td>";			
-		 	echo "<td>".$key->nombre."</td>";
-		 	echo "<td>".$key->apellido."</td>";
-		 	echo "<td>";
-		 	echo "<button id='$key->id' class='btnModificar btn btn-xs btn-warning'>Modificar</button>";
-		 	echo "&nbsp;";
-		 	echo "<a href='".base_url()."/portada/eliminar/".$key->id."' id='$key->id' class='btnEliminar btn btn-xs btn-danger'>Eliminar</a></td>";
-			echo "</tr>";
+<div id="resp">
+	<table class="table table-bordered" >
+		<thead class="bg-primary">
+			<th>#</th>
+			<th>Nombre</th>
+			<th>Apellido</th>
+			<th>-</th>		
+		</thead>
+		<tbody id="tabla-respuesta">
+			<?php 
+			foreach ($arr as $key) {
+				echo "<tr>";
+			 	echo "<td>".$key->id."</td>";			
+			 	echo "<td>".$key->nombre."</td>";
+			 	echo "<td>".$key->apellido."</td>";
+			 	echo "<td>";
+			 	echo "<button id='$key->id' class='btnModificar btn btn-xs btn-warning'>Modificar</button>";
+			 	echo "&nbsp;";
+			 	echo "<a href='".base_url()."/portada/eliminar/".$key->id."' id='$key->id' class='btnEliminar btn btn-xs btn-danger'>Eliminar</a></td>";
+				echo "</tr>";
 
-		 } ?>
-	</tbody>
-</table>
+			 } ?>
+		</tbody>
+	</table>
+	
+</div>
 
 
 
